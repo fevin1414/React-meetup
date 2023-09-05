a0d645f5cbc1e685965817270637f9cacab4134c
fevin1414 <fevinbiju1414@gmail.com>
2023-09-05 09:17:46 -0500

    added form

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,21 +1,32 @@
 import NewMeetupPage from "../../pages/NewMeetup";
-import classes from './newMeetupForm.module.css';
+import classes from "./newMeetupForm.module.css";
 import Card from "../ui/Card";
 
-
-function NewMeetupForm(){
-  return <Card>
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='title'>Meetup Title</label>
-        <input type="text" required id="title"/>
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='image'>Meetup Image</label>
-        <input type="url" required id="image"/>
-      </div>
-    </form>
-  </Card>
-
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" required rows="5"></textarea>
+        </div>
+        <div className={classes.action}>
+          <button>Add Meeting</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 export default NewMeetupForm;
