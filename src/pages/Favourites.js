ad030b435dda3c7602d2e224f5b81fd844ef7692
fevin1414 <fevinbiju1414@gmail.com>
2023-09-05 13:18:09 -0500

    displaying favourites

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,14 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favourite-context";
+import MeetupList from "../components/meetups/MeetupList";
 function Favourites() {
-  return <div>Favourites Page</div>;
+
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return<section>
+    <h1>Favourites </h1>
+    <MeetupList meetups={favoritesCtx.favorites}/>
+    </section>
 }
 
 export default Favourites;
